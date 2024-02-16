9f6fbb028f6097b09c721221a4782fdc63f7b3b0
GustavoNoronha0 <admin@gustavonoronha.com.br>
2024-02-16 11:24:01 -0300

    feat(grpc): add category client

---
+++ src/grpc/category/client.rs
@@ -0,0 +1,26 @@
+use category::category_client::CategoryClient;
+use category::GetCategoryRequest;
+use tonic::Request;
+use uuid::Uuid;
+
+pub mod category {
+    tonic::include_proto!("category");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = CategoryClient::connect("http://[::1]:50051").await?;
+
+    let category_id = Uuid::parse_str("any_id")?;
+
+    let request = tonic::Request::new(GetCategoryRequest {
+        id: category_id.to_string(),
+    });
+
+    println!("Sending request to gRPC Server...");
+    let response = client.get_category(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
+}
