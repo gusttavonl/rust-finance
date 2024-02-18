7c0cea73bbe1837b3bcef57e4d1bcd8e9fbdb68a
GustavoNoronha0 <admin@gustavonoronha.com.br>
2024-02-18 20:15:46 -0300

    feat: add categories migration

---
+++ migrations/20240213124531_categories.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS categories (
+    id UUID PRIMARY KEY NOT NULL DEFAULT (uuid_generate_v4()),
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(510) NOT NULL UNIQUE,
+    user_id UUID NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(id)
+);
